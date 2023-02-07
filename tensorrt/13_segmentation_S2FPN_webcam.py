93a346a7ed59d19fc4e173e7c3751c67af6c249a
maximofn <maximofn@gmail.com>
2023-02-07 08:14:40 +0100

    Se empieza el bucle con S2-FPN

---
+++ tensorrt/13_segmentation_S2FPN_webcam.py
@@ -0,0 +1,215 @@
+import torch
+from torch import nn
+import torchvision
+import torch.backends.cudnn as cudnn
+from torch2trt import torch2trt
+import cv2
+import numpy as np
+import time
+from thread import InputThread
+from udp_socket import udp_socket
+from video import video
+import segmentation_models_pytorch as smp
+# import matplotlib.pyplot as plt
+
+import sys
+sys.path.append('S2-FPN')
+from builders.model_builder import build_model
+
+# Create input thread
+input_thread = InputThread()
+input_thread.start()
+
+# Create UDP socket
+sock_frame = udp_socket('localhost', 8554, send=True)
+sock_mask = udp_socket('localhost', 8555, send=True)
+
+# Open webcam
+WEBCAM = True
+if not WEBCAM:
+    from PIL import Image
+CAPTURE_WIDTH = 1920
+CAPTURE_HEIGHT = 1080
+CAPTURE_FPS = 30
+video_frame = video(resize=False, width=CAPTURE_WIDTH, height=CAPTURE_HEIGHT, fps=CAPTURE_FPS, name="frame", display=False)
+video_mask = video(resize=False, width=CAPTURE_WIDTH, height=CAPTURE_HEIGHT, fps=CAPTURE_FPS, name="mask", display=False)
+video_frame.open(device=0)
+
+# Configuration of text on the screen
+font = cv2.FONT_HERSHEY_SIMPLEX
+fontScale = 0.3
+jump = 15
+fontColor = (0, 0, 255)#(10,10,10)
+lineThickness= 1
+lineType = cv2.LINE_AA
+
+# Time variables
+T0 = time.time()
+t_camera = 0
+t_preprocess = 0
+t_inference = 0
+t_postprocess = 0
+t_bucle = 0
+FPS = 0
+
+# Media variables
+iteracctions = -1
+t_read_frame_list = []
+t_img_gpu_list = []
+t_preprocess_list = []
+t_model_gpu_list = []
+t_inference_list = []
+t_postprocess_list = []
+t_bucle_list = []
+FPS_list = []
+
+# Custom model
+net="SSFPN" #model name: [DSANet,SPFNet,SSFPN]
+classes = 11 #number of classes
+def load_model(net, classes, gpu_id = 0): 
+    # os.environ["CUDA_VISIBLE_DEVICES"] = gpu_id
+    if not torch.cuda.is_available():
+        raise Exception("no GPU found or wrong gpu id")
+
+    # build the model
+    model = build_model(net, num_classes=classes)
+    model = model.cuda()  # using GPU for inference
+    # cudnn.benchmark = True
+
+    return model
+print("Creating model... ", end="")
+model = load_model(net, classes)
+model.eval()
+print("Done")
+
+# # convert to TensorRT feeding sample data as input
+# print("Converting to TensorRT")
+width = 360 #1920
+height = 360 #1088
+# x = np.ones((width, height, 3), dtype=np.uint8)
+# x = torch.from_numpy(x).float() # numpy to tensor
+# x = x.cuda()                    # move image to GPU
+# x = torch.flip(x, dims=[2])     # RGB to BGR
+# x = x / 255.0                   # 0-255 to 0-1
+# # x = x.permute(2, 0, 1)        # HWC to CHW
+# # x = x.unsqueeze(0)            # Add batch dimension
+# model_trt = torch2trt(model, [x])
+# print("Converted to TensorRT")
+
+while True:
+    t0 = time.time()
+    t_start = time.time()
+    if WEBCAM:
+        ret, frame = video_frame.read()
+    else:
+        # Read image S2-FPN/images/0001TP_006690.png
+        frame = cv2.imread('S2-FPN/images/0001TP_006690.png', cv2.IMREAD_COLOR)
+        img = frame.copy()
+        img = np.asarray(img, np.float32)
+        ret = True
+    t_camera = time.time() - t0
+    if not ret:
+        continue
+
+    # Preprocess image
+    t0 = time.time()
+    if WEBCAM:
+        img = cv2.resize(frame, (height, width))    # Resize
+        img = torch.from_numpy(img)                 # numpy to tensor
+        img = img.cuda()                            # move image to GPU
+        img = torch.flip(img, dims=[2])             # RGB to BGR
+        # img = img / 255.0                           # 0-255 to 0-1
+        img = img.type(torch.float32)             # uint8 to float32
+        img = img.permute(2, 0, 1)                # HWC to CHW
+        img = img.unsqueeze(0)                    # Add batch dimension
+    else:
+        img = img[:, :, ::-1]  # change to RGB
+        img = img.transpose((2, 0, 1)).copy()  # HWC -> CHW
+        img = torch.from_numpy(img)
+        img = img[None,:,:,:]
+        # img = img.cuda()
+        with torch.no_grad():
+            input_var = torch.autograd.Variable(img).cuda()
+    t_preprocess = time.time() - t0
+
+    # Inference
+    t0 = time.time()
+    # mask = model_trt(img)
+    if WEBCAM:
+        mask = model(img)
+        mask = mask[0].squeeze(0)
+    else:
+        mask = model(input_var)
+        torch.cuda.synchronize()
+    t_inference = time.time() - t0
+
+    # Postprocess
+    t0 = time.time()
+    if WEBCAM:
+        mask = torch.argmax(mask, axis=0).type(torch.uint8)
+        mask = mask.detach().cpu().numpy()
+    else:
+        mask = mask.cpu().data[0].numpy()
+        mask = mask.transpose(1, 2, 0)
+        mask = np.asarray(np.argmax(mask, axis=2), dtype=np.uint8)
+    t_postprocess = time.time() - t0
+
+    # Bucle time
+    t_bucle = time.time() - t_start
+
+    # FPS
+    FPS = 1 / t_bucle
+
+    # Put text
+    y = 30
+    cv2.putText(frame, f"Modelo en GPU:", (10, y), font, fontScale, fontColor, lineThickness, lineType); y += jump
+    cv2.putText(frame, f"FPS: {FPS:.2f}", (10, y), font, fontScale, fontColor, lineThickness, lineType); y += jump
+    cv2.putText(frame, f"Image shape: {frame.shape}, image type: {type(frame)}, img dtype: {frame.dtype}, img max: {frame.max()}, img min: {frame.min()}", (10, y), font, fontScale, fontColor, lineThickness, lineType); y += jump
+    cv2.putText(frame, f"Mask shape: {mask.shape}, mask type: {type(mask)}, mask dtype: {mask.dtype}, mask max: {mask.max():0.2f}, mask min: {mask.min():0.2f}", (10, y), font, fontScale, fontColor, lineThickness, lineType); y += jump
+    cv2.putText(frame, f"t camera: {t_camera*1000:.2f} ms", (10, y), font, fontScale, fontColor, lineThickness, lineType); y += jump
+    cv2.putText(frame, f"t preprocess: {t_preprocess*1000:.2f} ms", (10, y), font, fontScale, fontColor, lineThickness, lineType); y += jump
+    cv2.putText(frame, f"t inference: {t_inference*1000:.2f} ms", (10, y), font, fontScale, fontColor, lineThickness, lineType); y += jump
+    cv2.putText(frame, f"t postprocess: {t_postprocess*1000:.2f} ms", (10, y), font, fontScale, fontColor, lineThickness, lineType); y += jump
+    cv2.putText(frame, f"t bucle: {t_bucle*1000:.2f} ms", (10, y), font, fontScale, fontColor, lineThickness, lineType); y += jump
+
+    # Media variables
+    iteracctions += 1
+    if iteracctions >= 0:
+        t_read_frame_list.append(t_camera)
+        t_preprocess_list.append(t_preprocess)
+        t_inference_list.append(t_inference)
+        t_postprocess_list.append(t_postprocess)
+        t_bucle_list.append(t_bucle)
+        FPS_list.append(FPS)
+        cv2.putText(frame, f"Media: {iteracctions} iteracctions", (10, y), font, fontScale, fontColor, lineThickness, lineType); y += jump
+        cv2.putText(frame, f"    t read frame {np.mean(t_read_frame_list)*1000:.2f} ms", (10, y), font, fontScale, fontColor, lineThickness, lineType); y += jump
+        cv2.putText(frame, f"    t preprocess {np.mean(t_preprocess_list)*1000:.2f} ms,", (10, y), font, fontScale, fontColor, lineThickness, lineType); y += jump
+        cv2.putText(frame, f"    t inference {np.mean(t_inference_list)*1000:.2f} ms", (10, y), font, fontScale, fontColor, lineThickness, lineType); y += jump
+        cv2.putText(frame, f"    t postprocess {np.mean(t_postprocess_list)*1000:.2f} ms", (10, y), font, fontScale, fontColor, lineThickness, lineType); y += jump
+        cv2.putText(frame, f"    t bucle {np.mean(t_bucle_list)*1000:.2f} ms", (10, y), font, fontScale, fontColor, lineThickness, lineType); y += jump
+        cv2.putText(frame, f"    FPS {np.mean(FPS_list):.2f}", (10, y), font, fontScale, fontColor, lineThickness, lineType); y += jump
+
+    # Mandamos el frame por el socket
+    success, encoded_frame = video_frame.encode_frame(frame)
+    if success:
+        message = encoded_frame.tobytes(order='C')
+        sock_frame.send(message)
+
+    # Mandamos la máscara por el socket
+    success, encoded_mask = video_mask.encode_frame(mask)
+    if success:
+        message = encoded_mask.tobytes(order='C')
+        sock_mask.send(message)
+
+    # If user press type 'q' into the console in non blocking mode, exit
+    if input_thread.get_data() is not None and input_thread.get_data().strip() == 'q':
+        print("Se ha ha parado por el usuario")
+        input_thread.clear_data()
+        break
+
+
+# Cerramos el socket y la cámara
+sock_frame.close()
+sock_mask.close()
+video_frame.close()
+video_mask.close()
