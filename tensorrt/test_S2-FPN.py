29584e2356f2aa6b26a410a0fa4a7c8c6f61754f
maximofn <maximofn@gmail.com>
2023-02-07 08:55:51 +0100

    Se simplifica el test de S2-FPN

--- tensorrt/test_S2-FPN.py
+++ tensorrt/test_S2-FPN.py
@@ -6,8 +6,6 @@ import torch.backends.cudnn as cudnn
 from torch.autograd import Variable
 from argparse import ArgumentParser
 # user
-from builders.model_builder import build_model
-from builders.dataset_builder import build_dataset_test
 from PIL import Image
 
 import seaborn as sn
@@ -15,37 +13,19 @@ import matplotlib.pyplot as plt
 import glob
 import cv2
 
+import sys
+sys.path.append('S2-FPN')
+from builders.model_builder import build_model
+from builders.dataset_builder import build_dataset_test
+
 net="SSFPN" #model name: [DSANet,SPFNet,SSFPN]
-dataset="navig" #dataset: cityscapes or camvid
-num_workers=1 #the number of parallel threads
-batch_size=1 #the batch_size is set to 1 when evaluating or testing
-checkpoint_path = "./SSFPN18.pth" #use the file to load the checkpoint for evaluating or testing
-save_seg_dir ="./borrar/" #saving path of prediction result
-best = False #Get the best result among last few checkpoints
+checkpoint_path = "S2-FPN/weigths/SSFPN18.pth" #use the file to load the checkpoint for evaluating or testing
+save_seg_dir ="S2-FPN/borrar/" #saving path of prediction result
 save =True #Save the predicted image
-cuda=True #run on CPU or GPU
-gpus="0" #gpu ids (default: 0)
 classes = 11 #number of classes
-is_inference = True #weather you want to infer over images (True) or evaluate  the model (False)
-img_path = "./images/" #Images to be infered
+img_path = "S2-FPN/images/" #Images to be infered
 navig_palette = [170, 170, 170, 0, 255, 0, 255, 0, 0, 0, 120, 255, 0, 0, 255, 255,255,153]
 labels_names = ["path","travesable","no-travesable","sky","person","vehicle"]
-
-def generateM(gt,pred):
-    m = np.zeros((classes,classes))
-    assert (len(gt) == len(pred))
-    for i in range(len(gt)):
-        if gt[i] < classes:  # and pred[i] < self.nclass:
-            m[gt[i], pred[i]] += 1.0
-    return m
-
-def jaccard(cm):
-    jaccard_perclass = []
-    for i in range(classes):
-        #if not cm[i, i] == 0:
-        jaccard_perclass.append(cm[i, i] / (np.sum(cm[i, :]) + np.sum(cm[:, i]) - cm[i, i]))
-
-    return np.sum(jaccard_perclass) / len(jaccard_perclass), jaccard_perclass
 
 def colorize_mask(mask):
     # mask: numpy array of the mask
@@ -61,60 +41,20 @@ def save_predict(output, img_name,img_orig, save_path,overlay=0):
         result = cv2.addWeighted(img_orig,overlay,rgb_mask,1-overlay,0)
         cv2.imwrite(os.path.join(save_path, img_name + '_color.png'),result)
 
-def load_model(): 
-    if cuda:
-        print("=====> use gpu id: '{}'".format(gpus))
-        os.environ["CUDA_VISIBLE_DEVICES"] = gpus
-        if not torch.cuda.is_available():
-            raise Exception("no GPU found or wrong gpu id, please run without --cuda")
+def load_model(network): 
+    if not torch.cuda.is_available():
+        raise Exception("no GPU found or wrong gpu id, please run without --cuda")
 
     # build the model
-    model = build_model(net, num_classes=classes)
-    if cuda:
-        model = model.cuda()  # using GPU for inference
-        cudnn.benchmark = True
+    model = build_model(network, num_classes=classes)
+    model = model.cuda()  # using GPU for inference
+    cudnn.benchmark = True
 
     return model
-
-def test(test_loader, model,eval=False):
-    """
-    args:
-      test_loader: loaded for test dataset
-      model: model
-    return: class IoU and mean IoU
-    """
-    # evaluation or test mode
-    model.eval()
-    total_batches = len(test_loader)
-    
-    cm = np.zeros((classes, classes))
-
-    for i, (input, label, size, name) in enumerate(test_loader):
-        with torch.no_grad():
-            input_var = Variable(input).cuda()
-        start_time = time.time()
-        output = model(input_var) #,output5,output6,output7,output8
-        torch.cuda.synchronize()
-        time_taken = time.time() - start_time
-        #print('[%d/%d]  time: %.2f' % (i + 1, total_batches, time_taken))
-        output = output.cpu().data[0].numpy()
-        gt = np.asarray(label[0].numpy(), dtype=np.uint8)
-        output = output.transpose(1, 2, 0)
-        output = np.asarray(np.argmax(output, axis=2), dtype=np.uint8)
-        cm += generateM(gt.flatten(), output.flatten())
-        
-        # # save the predicted image
-        # if save and not best:
-        #     save_predict(output, gt, name[0].split("/")[-1],save_seg_dir,save, gt_color_save=False,overlay=False,img_orig=None)
-    
-    meanIoU, per_class_iu = jaccard(cm)
-
-    return meanIoU, per_class_iu, cm
 
 def inference(model):
     """
     args:
-      test_loader: loaded for test dataset
       model: model
     return: class IoU and mean IoU
     """
@@ -135,21 +75,16 @@ def inference(model):
         image = torch.from_numpy(image)
         image = image[None,:,:,:]
 
-
         with torch.no_grad():
             input_var = Variable(image).cuda()
         start_time = time.time()
         output = model(input_var) #,output5,output6,output7,output8
-        print(f"output shape: {output.shape}")
         torch.cuda.synchronize()
         time_taken = time.time() - start_time
-        print('[%d/%d]  time: %.2f' % (i + 1, n_ims, time_taken))
         output = output.cpu().data[0].numpy()
-        print(f"output shape: {output.shape}")
         output = output.transpose(1, 2, 0)
-        print(f"output shape: {output.shape}")
         output = np.asarray(np.argmax(output, axis=2), dtype=np.uint8)
-        print(f"output shape: {output.shape}")
+
         # save the predicted image
         if save:
             save_predict(output, name, img_orig, save_seg_dir,overlay=0.5)
@@ -160,78 +95,20 @@ def evaluate_model():
      param args: global arguments
      return: None
     """
-    model = load_model()
+    # build the model
+    print(f"Bulding model: {net}", end=" ")
+    model = load_model(net)
+    print("done")
 
-    # load the test set
-    # datas, testLoader = build_dataset_test(dataset, num_workers)
-
-    if not best or is_inference:
-        if os.path.isfile(checkpoint_path):
-            print("=====> loading checkpoint '{}'".format(checkpoint_path))
-            checkpoint = torch.load(checkpoint_path)
-            # print(f"checkpoint: {checkpoint}")
-            model.load_state_dict(checkpoint['model'])
-            # model.load_state_dict(convert_state_dict(checkpoint['model']))
-        else:
-            print("=====> no checkpoint found at '{}'".format(checkpoint_path))
-            raise FileNotFoundError("no checkpoint found at '{}'".format(checkpoint_path))
-
-        if is_inference: 
-            inference(model)
-        else:
-            
-            print("=====> beginning validation")
-            print("validation set length: ", len(testLoader))
-            mIOU_val, per_class_iu,cm = test(testLoader, model,eval = True)
-            print("=====> Mean IoU: ", str(mIOU_val))
-            print("=====> Per class IoU", str(per_class_iu))
-            sn.heatmap(cm.astype('float') / cm.sum(axis=1)[:, np.newaxis],annot=True,annot_kws={"size": 16},fmt=".2f",xticklabels=labels_names, yticklabels=labels_names)
-            plt.title("Confusion matrix")
-            plt.xlabel("Predictions")
-            plt.ylabel("True labels")
-
-    # Get the best test result among the last 10 model records.
+    print(f"loading checkpoint '{checkpoint_path}'", end=" ")
+    if os.path.isfile(checkpoint_path):
+        checkpoint = torch.load(checkpoint_path)
+        model.load_state_dict(checkpoint['model'])
+        print("done")
     else:
-        if os.path.isfile(checkpoint_path):
-            dirname, basename = os.path.split(checkpoint_path)
-            epoch = int(os.path.splitext(basename)[0].split('_')[1])
-            mIOU_val = []
-            per_class_iu = []
-            for i in range(epoch - 9, epoch+1):
-                basename = 'model_' + str(i) + '.pth'
-                resume = os.path.join(dirname, basename)
-                checkpoint = torch.load(resume)
-                model.load_state_dict(checkpoint['model'])
-                print("=====> beginning test the " + basename)
-                #print("validation set length: ", len(testLoader))
-                mIOU_val_0, per_class_iu_0,cm = test(testLoader, model,eval=True)
-                mIOU_val.append(mIOU_val_0)
-                per_class_iu.append(per_class_iu_0)
+        print(f"no checkpoint found at '{checkpoint_path}'")
+        raise FileNotFoundError("no checkpoint found at '{}'".format(checkpoint_path))
 
-            index = list(range(epoch - 9, epoch+1))[np.argmax(mIOU_val)]
-            print("The best mIoU among the last 10 models is", index)
-            print(mIOU_val)
-            per_class_iu = per_class_iu[np.argmax(mIOU_val)]
-            mIOU_val = np.max(mIOU_val)
-            print("=====> Best Mean IoU: ", str(mIOU_val))
-            print("=====> Best Per class IoU", str(per_class_iu))
-            sn.heatmap(cm.astype('float') / cm.sum(axis=1)[:, np.newaxis],annot=True,annot_kws={"size": 16},fmt=".2f",xticklabels=labels_names, yticklabels=labels_names)
-            plt.title("Confusion matrix")
-            plt.xlabel("Predictions")
-            plt.ylabel("True labels")
-            
-
-        else:
-            print("=====> no checkpoint found at '{}'".format(checkpoint_path))
-            raise FileNotFoundError("no checkpoint found at '{}'".format(checkpoint_path))
-
-    # Save the result
-    if not best:
-        model_path = os.path.splitext(os.path.basename(checkpoint_path))
-        logFile = 'test_' + model_path[0] + '.txt'
-        logFileLoc = os.path.join(os.path.dirname(checkpoint_path), logFile)
-    else:
-        logFile = 'test_' + 'best' + str(index) + '.txt'
-        logFileLoc = os.path.join(os.path.dirname(checkpoint_path), logFile)
+    inference(model)
 
 evaluate_model()
