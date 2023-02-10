b04fa5c28df11f70533c66848fe06f6b6d6ab708
maximofn <maximofn@gmail.com>
2023-02-10 13:31:09 +0100

    Se modifica la librería video para que ponga el tamaño del texto en los videos de acuerdo al tamaño del frame, además se arregla el overlay para que se pueda modificar este valor

--- tensorrt/video.py
+++ tensorrt/video.py
@@ -31,7 +31,7 @@ class video():
     
     def imshow(self, frame):
         # Print in botom of frame the frame shape
-        cv2.putText(frame, f"{self.name} shape: {frame.shape}", (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (179,147,97), 1)
+        cv2.putText(frame, f"{self.name} shape: {frame.shape}", (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, frame.shape[0]/1000, (10,147,255), 1)
         cv2.imshow(self.name, frame)
         if cv2.waitKey(1) == ord('q'):
             return False
@@ -46,11 +46,11 @@ class video():
         new_mask = new_mask[:, :, ::-1].copy()
         return new_mask
     
-    def overlay_mask(self, mask, img):
+    def overlay_mask(self, mask, img, overlay=0):
         # Overlay mask on image
         if img.shape[:2] != mask.shape[:2]:
             img = cv2.resize(img, (mask.shape[1], mask.shape[0]))
-        new_mask = cv2.addWeighted(img,0.5,mask,0.5,0)
+        new_mask = cv2.addWeighted(img,overlay,mask,1-overlay,0)
         return new_mask
     
     def maskshow(self, mask, img=None, colorize=False, overlay=False):
@@ -60,8 +60,8 @@ class video():
         else:
             mask_color = cv2.applyColorMap(mask, cv2.COLORMAP_JET)
         if overlay and img is not None:
-            mask_color = self.overlay_mask(mask_color, img)
-        cv2.putText(mask_color, f"{self.name} shape: {mask_color.shape}", (10, mask_color.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (179,147,97), 1)
+            mask_color = self.overlay_mask(mask_color, img, overlay)
+        cv2.putText(mask_color, f"{self.name} shape: {mask_color.shape}", (10, mask_color.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, mask.shape[0]/1000, (10,147,255), 1)
         cv2.imshow(self.name, mask_color)
         if cv2.waitKey(1) == ord('q'):
             return False
@@ -71,8 +71,8 @@ class video():
     def encode_frame(self, frame, format='.jpg'):
         return cv2.imencode(format, frame, [cv2.IMWRITE_JPEG_QUALITY,80])
     
-    def decode_frame(self, frame, codec='mp4v', fps=30):
-        return cv2.imdecode(self.frame, cv2.IMREAD_UNCHANGED)
+    def decode_frame(self, buffer, codec='mp4v', fps=30):
+        return cv2.imdecode(buffer, cv2.IMREAD_UNCHANGED)
     
     def close(self):
         if self.cap is not None:
