4c3a91c5b1b744cfb026aaedfbc4ea996c915eca
maximofn <maximofn@gmail.com>
2023-02-10 10:55:16 +0100

    Se modifica video.py para poder elegir la manera de colorear una máscara

--- tensorrt/video.py
+++ tensorrt/video.py
@@ -1,4 +1,6 @@
 import cv2
+from PIL import Image
+import numpy as np
 
 class video():
     def __init__(self, resize=False, width=1920, height=1080, fps=30, name="received frame", display=True):
@@ -10,6 +12,7 @@ class video():
         self.display = display
         self.cap = None
         if self.display: cv2.namedWindow(self.name, cv2.WINDOW_NORMAL)
+        self.navig_palette = [170, 170, 170, 0, 255, 0, 255, 0, 0, 0, 120, 255, 0, 0, 255, 255,255,153]
     
     def open(self, device=0):
         self.cap = cv2.VideoCapture(device)
@@ -34,11 +37,31 @@ class video():
             return False
         else:
             return True
+
+    def colorized_mask(self, mask, img=None, overlay=0):
+        # Colorize mask
+        new_mask = Image.fromarray(mask.astype(np.uint8)).convert('P')
+        new_mask.putpalette(self.navig_palette)
+        new_mask = np.array(new_mask.convert("RGB"))
+        new_mask = new_mask[:, :, ::-1].copy()
+        return new_mask
     
-    def maskshow(self, mask):
+    def overlay_mask(self, mask, img):
+        # Overlay mask on image
+        if img.shape[:2] != mask.shape[:2]:
+            img = cv2.resize(img, (mask.shape[1], mask.shape[0]))
+        new_mask = cv2.addWeighted(img,0.5,mask,0.5,0)
+        return new_mask
+    
+    def maskshow(self, mask, img=None, colorize=False, overlay=False):
         # Print in botom of mask the mask shape
-        cv2.putText(mask, f"{self.name} shape: {mask.shape}", (10, mask.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (179,147,97), 1)
-        mask_color = cv2.applyColorMap(mask, cv2.COLORMAP_JET)
+        if colorize:
+            mask_color = self.colorized_mask(mask, img, overlay)
+        else:
+            mask_color = cv2.applyColorMap(mask, cv2.COLORMAP_JET)
+        if overlay and img is not None:
+            mask_color = self.overlay_mask(mask_color, img)
+        cv2.putText(mask_color, f"{self.name} shape: {mask_color.shape}", (10, mask_color.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (179,147,97), 1)
         cv2.imshow(self.name, mask_color)
         if cv2.waitKey(1) == ord('q'):
             return False
